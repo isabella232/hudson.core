195db4f74e860eb7bcbd8b666b22ffd361dd29d2
Winston Prakash <winston.prakash@gmail.com>
2011-10-06 17:12:38 -0700

    Initial checkin of hudson-war module with out icons and javascript (CQ - 5515)

---
+++ hudson-war/src/main/webapp/WEB-INF/security/AbstractPasswordBasedSecurityRealm.groovy
@@ -0,0 +1,39 @@
+/**************************************************************************
+#
+# Copyright (C) 2004-2010 Oracle Corporation
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#      Kohsuke Kawaguchi
+#
+#**************************************************************************/ 
+/*
+    Configure Hudson's own user database as the authentication realm.
+*/
+import org.acegisecurity.providers.ProviderManager
+import org.acegisecurity.providers.anonymous.AnonymousAuthenticationProvider
+import org.acegisecurity.providers.rememberme.RememberMeAuthenticationProvider
+import hudson.model.Hudson
+
+authenticationManager(ProviderManager) {
+    providers = [
+        // this does all the hard work.
+        // injected by the parsing code
+        authenticator,
+
+    // these providers apply everywhere
+        bean(RememberMeAuthenticationProvider) {
+            key = Hudson.getInstance().getSecretKey();
+        },
+        // this doesn't mean we allow anonymous access.
+        // we just authenticate anonymous users as such,
+        // so that later authorization can reject them if so configured
+        bean(AnonymousAuthenticationProvider) {
+            key = "anonymous"
+        }
+    ]
+}
