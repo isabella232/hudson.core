c42e4c7be397a15854ed1cc6c4de6ec7f3630d68
Winston Prakash <winston.prakash@gmail.com>
2011-11-16 18:12:51 -0800

    Add more attributes like graph type, label visibility, label position etc

--- hudson-core/src/main/java/hudson/util/graph/GraphSeries.java
+++ hudson-core/src/main/java/hudson/util/graph/GraphSeries.java
@@ -14,27 +14,59 @@ import java.util.List;
  */
 public class GraphSeries<E> {
 
+    public static final int TYPE_AREA = 1;
+    public static final int TYPE_BAR = 2;
+    public static final int TYPE_LINE = 3;
+    
     private Color color;
+    
+    /** Caption for the series to display in the legend */
     private String caption;
-    //Base URL to construct full URL for data Click on Graph
+    
+    /** Base URL to construct full URL for data Click on Graph */
     private String baseURL;
+    
+    /** The series data */
     private List<E> data = new ArrayList<E>();
-    // Should the label displayed inside the data point display area
-    private boolean labelInside = false;
-
-    public GraphSeries(String label) {
-        this.caption = label;
+    
+    /** Should the label displayed inside the data point display area useful in Bar, Area */
+    private boolean labelInside = true;
+    
+    /** How this series should be displayed - BAR, AREA, LINE etc */
+    private int type = TYPE_BAR;
+    
+    /** Should the value label be displayed */
+    private boolean valueLabelDisplayed = true;
+    
+    /** Should this chart series be stacked over previous series*/
+    private boolean stacked = true;
+    
+    public GraphSeries(String caption){
+        this.caption = caption;
     }
     
-    public GraphSeries(String label, Color color) {
-        this.caption = label;
-        this.color = color;
+    public GraphSeries(int type, String caption, Color color){
+        this(type, caption, color, true, true);
     }
     
-    public GraphSeries(String label, Color color, boolean labelInside) {
-        this.caption = label;
+    public GraphSeries(int type, String caption, Color color, boolean valueLabelDisplayed) {
+        this(type, caption, color, valueLabelDisplayed, true);
+    }
+    
+    public GraphSeries(int type, String caption, Color color, boolean valueLabelDisplayed, boolean labelInside) {
+        this.type = type; 
+        this.caption = caption;
         this.color = color;
+        this.valueLabelDisplayed = valueLabelDisplayed;
         this.labelInside = labelInside;
+    }
+    
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 
     public List<E> getData() {
@@ -73,11 +105,27 @@ public class GraphSeries<E> {
         data.add(element);
     }
     
+     public boolean isStacked() {
+        return stacked;
+    }
+
+    public void setStacked(boolean stacked) {
+        this.stacked = stacked;
+    }
+    
     public boolean isLabelInside() {
         return labelInside;
     }
 
     public void setLabelInside(boolean labelInside) {
         this.labelInside = labelInside;
+    }
+    
+    public boolean isValueLabelDisplayed() {
+        return valueLabelDisplayed;
+    }
+
+    public void setValueLabelDisplayed(boolean valueLabelDisplayed) {
+        this.valueLabelDisplayed = valueLabelDisplayed;
     }
 }
