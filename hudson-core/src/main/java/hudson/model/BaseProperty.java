29169f0c536e93587dc884eddd6a89daf7530df7
akozak <antkozak@gmail.com>
2011-11-22 11:50:28 +0300

    Replace property_name enum with simple constants. Add javadocs. Introduce BaseProperty class which contains common methods for IProperty handling
    Signed-off-by: Winston Prakash <winston.prakash@gmail.com>

---
+++ hudson-core/src/main/java/hudson/model/BaseProperty.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2011 Oracle Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *
+ *    Nikita Levyankov
+ *
+ *******************************************************************************/
+
+package hudson.model;
+
+import java.io.IOException;
+import org.eclipse.hudson.api.model.IJob;
+import org.eclipse.hudson.api.model.IProperty;
+
+/**
+ * Base property implementation. Contains common methods for setting and getting cascading and overridden properties.
+ * <p/>
+ * Date: 9/22/11
+ *
+ * @author Nikita Levyankov
+ */
+public abstract class BaseProperty<T> implements IProperty<T> {
+
+    private String propertyKey;
+    private transient IJob job;
+    private T originalValue;
+    private boolean propertyOverridden;
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setKey(String propertyKey) {
+        this.propertyKey = propertyKey;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setJob(IJob job) {
+        assert job != null;
+        this.job = job;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public T getCascadingValue() throws IOException {
+        return job.hasCascadingProject() ?
+            (T) job.getCascadingProject().getProperty(propertyKey, this.getClass()).getValue() : null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isPropertyOverridden() {
+        return propertyOverridden;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public T getValue() throws IOException {
+        if (isPropertyOverridden() || null != getOriginalValue()) {
+            return getOriginalValue();
+        }
+        return getCascadingValue();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public void setValue(T value) throws IOException {
+        value = prepareValue(value);
+        if (!job.hasCascadingProject()) {
+            originalValue = value;
+        } else if (allowOverrideValue(
+            (T) job.getCascadingProject().getProperty(propertyKey, this.getClass()).getValue(), value)) {
+            originalValue = value;
+            propertyOverridden = true;
+        } else {
+            this.originalValue = null;
+            propertyOverridden = false;
+        }
+    }
+
+    /**
+     * Returns true, if cascading value should be overridden by candidate value.
+     *
+     * @param cascadingValue value from cascading project if any.
+     * @param candidateValue candidate value.
+     * @return true if cascading value should be replaced by candidate value.
+     */
+    protected abstract boolean allowOverrideValue(T cascadingValue, T candidateValue);
+
+    /**
+     * Pre-process candidate value.
+     *
+     * @param candidateValue candidateValue.
+     * @return candidateValue by default.
+     */
+    protected T prepareValue(T candidateValue) {
+        return candidateValue;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public T getOriginalValue() {
+        return originalValue;
+    }
+}
