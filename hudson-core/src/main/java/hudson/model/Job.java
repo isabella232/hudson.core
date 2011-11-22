ea71dba4b0b446b02b7a85ad011c43de2c3b1fcf
akozak <antkozak@gmail.com>
2011-11-22 10:02:42 +0300

    "Update licenses. Rename isTemplateValue=>isCascadingValue. Fixed cobertura configuration
    Signed-off-by: Winston Prakash <winston.prakash@gmail.com>

--- hudson-core/src/main/java/hudson/model/Job.java
+++ hudson-core/src/main/java/hudson/model/Job.java
@@ -1275,7 +1275,7 @@ public abstract class Job<JobT extends Job<JobT, RunT>, RunT extends Run<JobT, R
      * Checks whether current job is inherited from other project.
      * @return boolean.
      */
-    protected boolean hasCascadingProject() {
+    public boolean hasCascadingProject() {
         return null != getCascadingProject();
     }
 
