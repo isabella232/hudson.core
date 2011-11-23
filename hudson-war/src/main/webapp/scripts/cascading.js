7434e8b93338bff3435b9758da07ec294824b153
akozak <antkozak@gmail.com>
2011-11-23 09:24:53 +0300

    Draft implementation of revert functionality for optionalBlock element. Add License to cascading.js
    Signed-off-by: Winston Prakash <winston.prakash@gmail.com>

--- hudson-war/src/main/webapp/scripts/cascading.js
+++ hudson-war/src/main/webapp/scripts/cascading.js
@@ -1,3 +1,36 @@
+
+/*******************************************************************************
+ *
+ * Copyright (c) 2004-2010 Oracle Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *
+ *    Nikita Levyankov, Anton Kozak
+ *
+ *******************************************************************************/
+
+hudsonRules["A.reset-button"] = function(e) {
+    e.onclick = function() {
+        new Ajax.Request(this.getAttribute("resetURL"), {
+                method : 'get',
+                onSuccess : function(x) {
+                    location.reload(true);
+                },
+                onFailure : function(x) {
+
+                }
+            });
+        return false;
+    }
+    e.tabIndex = 9999; // make help link unnavigable from keyboard
+    e = null; // avoid memory leak
+}
+
 jQuery(document).ready(function(){
     jQuery('select[name=cascadingProjectName]').change(function() {
         var url = window.location.href;
