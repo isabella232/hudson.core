6dc25a8df9150ce6bd444daecc5b72893b6cb938
akozak <antkozak@gmail.com>
2011-11-24 16:19:07 +0300

    addPublisher/removePublisher methods were relocated to BaseBuildableProejct.
    Signed-off-by: Winston Prakash <winston.prakash@gmail.com>

--- hudson-core/src/test/java/org/eclipse/hudson/api/model/project/property/ProjectPropertyTest.java
+++ hudson-core/src/test/java/org/eclipse/hudson/api/model/project/property/ProjectPropertyTest.java
@@ -712,6 +712,7 @@ public class ProjectPropertyTest {
         TriggerProjectProperty property = new TriggerProjectProperty(project);
         Trigger originalTrigger = new TimerTrigger("* * * * *");
         Trigger cascadingTrigger = new TimerTrigger("* * * * *");
+        property.setModified(true);
         property.updateOriginalValue(originalTrigger, cascadingTrigger);
         //Property isn't overridden because of values equal.
         assertFalse(property.isOverridden());
