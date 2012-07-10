086424616eb3b238fd7f980ab1591e88fc185169
Henrik Lynggaard Hansen <henrik@hlyh.dk>
2012-07-10 21:18:41 +0200

    Reformat of diagnosis and fsp package.
    Change-Id: I04387473d384e829ae2924671cc9de52a1721f0a
    Signed-off-by: Henrik Lynggaard Hansen <henrik@hlyh.dk>

--- hudson-core/src/main/java/hudson/diagnosis/MemoryUsageMonitor.java
+++ hudson-core/src/main/java/hudson/diagnosis/MemoryUsageMonitor.java
@@ -7,10 +7,10 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: 
+ * Contributors:
  *
  *    Kohsuke Kawaguchi, Winston Prakash
- *     
+ *
  *******************************************************************************/ 
 
 package hudson.diagnosis;
@@ -40,26 +40,28 @@ import org.kohsuke.stapler.QueryParameter;
  */
 @Extension
 public final class MemoryUsageMonitor extends PeriodicWork {
+
     /**
-     * A memory group is conceptually a set of memory pools. 
+     * A memory group is conceptually a set of memory pools.
      */
     public final class MemoryGroup {
-        private final List<MemoryPoolMXBean> pools = new ArrayList<MemoryPoolMXBean>();
 
+        private final List<MemoryPoolMXBean> pools = new ArrayList<MemoryPoolMXBean>();
         /**
-         * Trend of the memory usage, after GCs.
-         * So this shows the accurate snapshot of the footprint of live objects.
+         * Trend of the memory usage, after GCs. So this shows the accurate
+         * snapshot of the footprint of live objects.
          */
-        public final MultiStageTimeSeries used = new MultiStageTimeSeries(Messages._MemoryUsageMonitor_USED(), ColorPalette.RED, 0,0);
+        public final MultiStageTimeSeries used = new MultiStageTimeSeries(Messages._MemoryUsageMonitor_USED(), ColorPalette.RED, 0, 0);
         /**
          * Trend of the maximum memory size, after GCs.
          */
-        public final MultiStageTimeSeries max = new MultiStageTimeSeries(Messages._MemoryUsageMonitor_TOTAL(), ColorPalette.BLUE, 0,0);
+        public final MultiStageTimeSeries max = new MultiStageTimeSeries(Messages._MemoryUsageMonitor_TOTAL(), ColorPalette.BLUE, 0, 0);
 
         private MemoryGroup(List<MemoryPoolMXBean> pools, MemoryType type) {
             for (MemoryPoolMXBean pool : pools) {
-                if (pool.getType() == type)
+                if (pool.getType() == type) {
                     this.pools.add(pool);
+                }
             }
         }
 
@@ -69,9 +71,11 @@ public final class MemoryUsageMonitor extends PeriodicWork {
 //            long cur = 0;
             for (MemoryPoolMXBean pool : pools) {
                 MemoryUsage usage = pool.getCollectionUsage();
-                if(usage==null) continue;   // not available
+                if (usage == null) {
+                    continue;   // not available
+                }
                 used += usage.getUsed();
-                max  += usage.getMax();
+                max += usage.getMax();
 
 //                usage = pool.getUsage();
 //                if(usage==null) continue;   // not available
@@ -93,10 +97,9 @@ public final class MemoryUsageMonitor extends PeriodicWork {
          * Generates the memory usage statistics graph.
          */
         public TrendChart doGraph(@QueryParameter String type) throws IOException {
-            return MultiStageTimeSeries.createTrendChart(TimeScale.parse(type),used,max);
+            return MultiStageTimeSeries.createTrendChart(TimeScale.parse(type), used, max);
         }
     }
-
     public final MemoryGroup heap;
     public final MemoryGroup nonHeap;
 
--- hudson-core/src/main/java/hudson/diagnosis/ReverseProxySetupMonitor.java
+++ hudson-core/src/main/java/hudson/diagnosis/ReverseProxySetupMonitor.java
@@ -7,10 +7,10 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: 
+ * Contributors:
  *
- *   
- *       
+ *
+ *
  *
  *******************************************************************************/ 
 
@@ -28,18 +28,21 @@ import org.kohsuke.stapler.WebMethod;
 import java.io.IOException;
 
 /**
- * Looks out for a broken reverse proxy setup that doesn't rewrite the location header correctly.
+ * Looks out for a broken reverse proxy setup that doesn't rewrite the location
+ * header correctly.
  *
- * <p>
- * Have the JavaScript make an AJAX call, to which we respond with 302 redirect. If the reverse proxy
- * is done correctly, this will be handled by {@link #doFoo()}, but otherwise we'll report that as an error.
- * Unfortunately, {@code XmlHttpRequest} doesn't expose properties that allow the client-side JavaScript
- * to learn the details of the failure, so we have to make do with limited information.
+ * <p> Have the JavaScript make an AJAX call, to which we respond with 302
+ * redirect. If the reverse proxy is done correctly, this will be handled by
+ * {@link #doFoo()}, but otherwise we'll report that as an error. Unfortunately,
+ * {@code XmlHttpRequest} doesn't expose properties that allow the client-side
+ * JavaScript to learn the details of the failure, so we have to make do with
+ * limited information.
  *
  * @author Kohsuke Kawaguchi
  */
 @Extension
 public class ReverseProxySetupMonitor extends AdministrativeMonitor {
+
     @Override
     public boolean isActivated() {
         // return true to always inject an HTML fragment to perform a test
@@ -50,16 +53,17 @@ public class ReverseProxySetupMonitor extends AdministrativeMonitor {
         return new HttpRedirect("test-for-reverse-proxy-setup");
     }
 
-    @WebMethod(name="test-for-reverse-proxy-setup")
+    @WebMethod(name = "test-for-reverse-proxy-setup")
     public FormValidation doFoo() {
         return FormValidation.ok();
     }
 
     /**
-     * Depending on whether the user said "yes" or "no", send him to the right place.
+     * Depending on whether the user said "yes" or "no", send him to the right
+     * place.
      */
     public HttpResponse doAct(@QueryParameter String no) throws IOException {
-        if(no!=null) { // dismiss
+        if (no != null) { // dismiss
             disable(true);
             // of course the irony is that this redirect won't work
             return HttpResponses.redirectViaContextPath("/manage");
@@ -68,4 +72,3 @@ public class ReverseProxySetupMonitor extends AdministrativeMonitor {
         }
     }
 }
-
