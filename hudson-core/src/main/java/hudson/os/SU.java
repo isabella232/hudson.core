bbaac1367e66ef1a4a698664cabecd29abbb6fe4
Henrik Lynggaard Hansen <henrik@hlyh.dk>
2012-07-17 21:55:51 +0200

    Reformat hudson.os and hudson.scheduler
    Change-Id: If5f3ff060effd6a250969abf44d42ad34a3d6ec9
    Signed-off-by: Henrik Lynggaard Hansen <henrik@hlyh.dk>

--- hudson-core/src/main/java/hudson/os/SU.java
+++ hudson-core/src/main/java/hudson/os/SU.java
@@ -7,10 +7,10 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: 
+ * Contributors:
  *
  *    Kohsuke Kawaguchi
- *     
+ *
  *
  *******************************************************************************/ 
 
@@ -36,43 +36,45 @@ import java.io.PrintStream;
 import java.util.Collections;
 
 /**
- * Executes {@link Callable} as the super user, by forking a new process and executing the closure in there
- * if necessary.
+ * Executes {@link Callable} as the super user, by forking a new process and
+ * executing the closure in there if necessary.
  *
- * <p>
- * A best effort is made to execute the closure as root, but we may still end up exeucting the closure
- * in the non-root privilege, so the closure should expect that and handle it gracefully.
+ * <p> A best effort is made to execute the closure as root, but we may still
+ * end up exeucting the closure in the non-root privilege, so the closure should
+ * expect that and handle it gracefully.
  *
- * <p>
- * Still very much experimental. Subject to change. <b>Don't use it.</b>
+ * <p> Still very much experimental. Subject to change. <b>Don't use it.</b>
  *
  * @author Kohsuke Kawaguchi
  */
 public abstract class SU {
+
     private SU() { // not meant to be instantiated
     }
 
     /**
-     * Returns a {@link VirtualChannel} that's connected to the priviledge-escalated environment.
+     * Returns a {@link VirtualChannel} that's connected to the
+     * priviledge-escalated environment.
      *
-     * @return
-     *      Never null. This may represent a channel to a separate JVM, or just {@link LocalChannel}.
-     *      Close this channel and the SU environment will be shut down.
+     * @return Never null. This may represent a channel to a separate JVM, or
+     * just {@link LocalChannel}. Close this channel and the SU environment will
+     * be shut down.
      */
     public static VirtualChannel start(final TaskListener listener, final String rootUsername, final String rootPassword) throws IOException, InterruptedException {
-        if(File.pathSeparatorChar==';') // on Windows
+        if (File.pathSeparatorChar == ';') // on Windows
+        {
             return newLocalChannel();  // TODO: perhaps use RunAs to run as an Administrator?
-
+        }
         String os = Util.fixNull(System.getProperty("os.name"));
-        if(os.equals("Linux"))
+        if (os.equals("Linux")) {
             return new UnixSu() {
                 protected String sudoExe() {
                     return "sudo";
                 }
 
                 protected Process sudoWithPass(ArgumentListBuilder args) throws IOException {
-                    args.prepend(sudoExe(),"-S");
-                    listener.getLogger().println("$ "+Util.join(args.toList()," "));
+                    args.prepend(sudoExe(), "-S");
+                    listener.getLogger().println("$ " + Util.join(args.toList(), " "));
                     ProcessBuilder pb = new ProcessBuilder(args.toCommandArray());
                     Process p = pb.start();
                     // TODO: use -p to detect prompt
@@ -83,9 +85,10 @@ public abstract class SU {
                     ps.println(rootPassword);
                     return p;
                 }
-            }.start(listener,rootPassword);
+            }.start(listener, rootPassword);
+        }
 
-        if(os.equals("SunOS"))
+        if (os.equals("SunOS")) {
             return new UnixSu() {
                 protected String sudoExe() {
                     return "/usr/bin/pfexec";
@@ -96,9 +99,10 @@ public abstract class SU {
                     ProcessBuilder pb = new ProcessBuilder(args.prepend(sudoExe()).toCommandArray());
                     return EmbeddedSu.startWithSu(rootUsername, rootPassword, pb);
                 }
-            // in solaris, pfexec never asks for a password, so username==null means
-            // we won't be using password. this helps disambiguate empty password
-            }.start(listener,rootUsername==null?null:rootPassword);
+                // in solaris, pfexec never asks for a password, so username==null means
+                // we won't be using password. this helps disambiguate empty password
+            }.start(listener, rootUsername == null ? null : rootPassword);
+        }
 
         // TODO: Mac?
 
@@ -111,9 +115,10 @@ public abstract class SU {
     }
 
     /**
-     * Starts a new priviledge-escalated environment, execute a closure, and shut it down.
+     * Starts a new priviledge-escalated environment, execute a closure, and
+     * shut it down.
      */
-    public static <V,T extends Throwable> V execute(TaskListener listener, String rootUsername, String rootPassword, final Callable<V, T> closure) throws T, IOException, InterruptedException {
+    public static <V, T extends Throwable> V execute(TaskListener listener, String rootUsername, String rootPassword, final Callable<V, T> closure) throws T, IOException, InterruptedException {
         VirtualChannel ch = start(listener, rootUsername, rootPassword);
         try {
             return ch.call(closure);
@@ -138,19 +143,23 @@ public abstract class SU {
                 throw new IOException(exc);
             }
 
-            if(uid==0)  // already running as root
+            if (uid == 0) // already running as root
+            {
                 return newLocalChannel();
+            }
 
             String javaExe = System.getProperty("java.home") + "/bin/java";
             File slaveJar = Which.jarFile(Launcher.class);
 
             ArgumentListBuilder args = new ArgumentListBuilder().add(javaExe);
-            if(slaveJar.isFile())
+            if (slaveJar.isFile()) {
                 args.add("-jar").add(slaveJar);
-            else // in production code this never happens, but during debugging this is convenientud    
+            } else // in production code this never happens, but during debugging this is convenientud    
+            {
                 args.add("-cp").add(slaveJar).add(hudson.remoting.Launcher.class.getName());
+            }
 
-            if(rootPassword==null) {
+            if (rootPassword == null) {
                 // try sudo, in the hope that the user has the permission to do so without password
                 return new LocalLauncher(listener).launchChannel(
                         args.prepend(sudoExe()).toCommandArray(),
@@ -159,7 +168,7 @@ public abstract class SU {
                 // try sudo with the given password. Also run in pfexec so that we can elevate the privileges
                 Process proc = sudoWithPass(args);
                 return Channels.forProcess(args.toStringWithQuote(), Computer.threadPoolForRemoting, proc,
-                        listener.getLogger() );
+                        listener.getLogger());
             }
         }
     }
