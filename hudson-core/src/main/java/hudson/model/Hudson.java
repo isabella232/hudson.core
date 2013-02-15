c34f423ee2208cd33bc3101246ee911ff461e351
Winston Prakash <winston.prakash@gmail.com>
2013-02-15 13:16:24 -0800

    Started implementing the Team Concept. This is the initial pass, which just groups the jobs in the dashboard based on logged in user

--- hudson-core/src/main/java/hudson/model/Hudson.java
+++ hudson-core/src/main/java/hudson/model/Hudson.java
@@ -204,6 +204,7 @@ import org.eclipse.hudson.plugins.PluginCenter;
 import org.eclipse.hudson.script.ScriptSupport;
 import org.eclipse.hudson.security.HudsonSecurityEntitiesHolder;
 import org.eclipse.hudson.security.HudsonSecurityManager;
+import org.eclipse.hudson.security.team.TeamManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -393,6 +394,7 @@ public final class Hudson extends Node implements ItemGroup<TopLevelItem>, Stapl
      */
     public transient final PluginManager pluginManager;
     private transient PluginCenter pluginCenter;
+    private transient TeamManager teamManager;
     public transient volatile TcpSlaveAgentListener tcpSlaveAgentListener;
     private transient UDPBroadcastThread udpBroadcastThread;
     private transient DNSMultiCast dnsMultiCast;
@@ -574,6 +576,8 @@ public final class Hudson extends Node implements ItemGroup<TopLevelItem>, Stapl
             this.pluginManager = pluginManager;
 
             pluginCenter = new PluginCenter(root);
+            
+            teamManager = new TeamManager(root);
 
             // JSON binding needs to be able to see all the classes from all the plugins
             WebApp.get(servletContext).setClassLoader(pluginManager.uberClassLoader);
@@ -772,6 +776,10 @@ public final class Hudson extends Node implements ItemGroup<TopLevelItem>, Stapl
 
     public PluginCenter getPluginCenter() {
         return pluginCenter;
+    }
+    
+    public TeamManager getTeamManager() {
+        return teamManager;
     }
 
     public HudsonSecurityManager getSecurityManager() {
@@ -1239,7 +1247,9 @@ public final class Hudson extends Node implements ItemGroup<TopLevelItem>, Stapl
         List<TopLevelItem> viewableItems = new ArrayList<TopLevelItem>();
         for (TopLevelItem item : items.values()) {
             if (item.hasPermission(Item.READ)) {
-                viewableItems.add(item);
+                if (teamManager.isCurrentUserHasAccess(item.getName())) {
+                    viewableItems.add(item);
+                }
             }
         }
 
@@ -2084,6 +2094,10 @@ public final class Hudson extends Node implements ItemGroup<TopLevelItem>, Stapl
         if (item == null || !item.hasPermission(Item.READ)) {
             return null;
         }
+        if (!teamManager.isCurrentUserHasAccess(item.getName())) {
+            return null;
+        }
+        
         return item;
     }
 
