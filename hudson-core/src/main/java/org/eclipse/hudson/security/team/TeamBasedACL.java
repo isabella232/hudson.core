3650dcf92d2a009d12ff0352a17be1a820234353
Winston Prakash <winston.prakash@gmail.com>
2013-04-26 22:41:20 -0700

    Add Team based authorization strategy for proper authorization for Team concept

---
+++ hudson-core/src/main/java/org/eclipse/hudson/security/team/TeamBasedACL.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2013 Oracle Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Winston Prakash
+ */
+package org.eclipse.hudson.security.team;
+
+import hudson.model.Item;
+import hudson.model.Job;
+import hudson.security.Permission;
+import hudson.security.SidACL;
+import org.eclipse.hudson.security.team.TeamManager.TeamNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.acls.sid.Sid;
+
+/**
+ * Team based authorization
+ *
+ * @since 3.1.0
+ * @author Winston Prakash
+ */
+public class TeamBasedACL extends SidACL {
+    
+    private transient Logger logger = LoggerFactory.getLogger(TeamBasedACL.class);
+
+    public enum SCOPE {
+
+        GLOBAL, TEAM, JOB
+    };
+    private final SCOPE scope;
+    private final TeamManager teamManager;
+    private Job job;
+    private Team team;
+
+    public TeamBasedACL(TeamManager teamManager, SCOPE scope) {
+        this.teamManager = teamManager;
+        this.scope = scope;
+    }
+
+    public TeamBasedACL(TeamManager teamManager, SCOPE scope, Job job) {
+        this(teamManager, scope);
+        this.job = job;
+    }
+
+    public TeamBasedACL(TeamManager teamManager, SCOPE scope, Team team) {
+        this(teamManager, scope);
+        this.team = team;
+    }
+
+    @Override
+    protected Boolean hasPermission(Sid sid, Permission permission) {
+        String userName = toString(sid);
+        
+        // SysAdmin gets all permission
+        if (teamManager.isSysAdmin(userName)) {
+                return true;
+        } 
+        if (scope == SCOPE.GLOBAL) {
+            //All non team members gets only READ Permission
+            if (permission.getImpliedBy() == Permission.READ) {
+                return true;
+            }
+            // Member of any of the team get JOB CREATE Permission
+            if (permission == Item.CREATE) {
+                Team team = teamManager.findUserTeam(userName);
+                if (team != null) {
+                    return true;
+                }
+            }
+        }
+        if (scope == SCOPE.TEAM) {
+            // Sysadmin gets to do all team maintenance operations
+            if (teamManager.isSysAdmin(userName)) {
+                return true;
+            }
+            Team team = teamManager.findUserTeam(userName);
+
+            if (team != null) {
+                // Team admin gets to do all team maintenance operations
+                if (team.isAdmin(userName)) {
+                    return true;
+                } else if (team.isMember(userName) && permission.getImpliedBy() == Permission.READ) {
+                    return true;
+                }
+            }
+        }
+        if (scope == SCOPE.JOB) {
+            Team jobTeam = teamManager.findJobOwnerTeam(job.getName());
+
+            if (jobTeam != null) {
+                if (jobTeam.isMember(userName)) {
+                    // Team admin gets full permission on all jobs
+                    if (jobTeam.isAdmin(userName)) {
+                        return true;
+                    } else {
+                        //TODO:
+                        //    Check permissions based on job scope, 
+                        //    Grant all permission to job owner if it is marked private
+                        //    No permission to all other team members for private jobs
+                        //    Grant member specific permissions 
+
+                        return true; // for now give full permission to all team members
+                    }
+                }
+            }
+            // Grant Read permission to 
+            Team defaultTeam;
+            try {
+                defaultTeam = teamManager.findTeam("default");
+
+                if (defaultTeam.isJobOwner(job.getName())) {
+                    if (permission.getImpliedBy() == Permission.READ) {
+                        return true;
+                    } 
+                }
+            } catch (TeamNotFoundException ex) {
+                logger.error("The deafult team must exists.", ex);
+            }
+            // TODO: Grant read permission to jobs marked as public scoped in all teams
+        }
+        return false;
+
+    }
+}
---
+++ hudson-core/src/main/java/org/eclipse/hudson/security/team/TeamBasedAuthorizationStrategy.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2013 Oracle Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Winston Prakash
+ */
+package org.eclipse.hudson.security.team;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.model.Job;
+import hudson.security.ACL;
+import hudson.security.AuthorizationStrategy;
+import hudson.security.GlobalMatrixAuthorizationStrategy;
+import java.util.Collection;
+import java.util.Collections;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Team based authorization strategy
+ *
+ * @since 3.1.0
+ * @author Winston Prakash
+ */
+public class TeamBasedAuthorizationStrategy extends AuthorizationStrategy {
+
+    private transient final TeamManager teamManager;
+
+    @DataBoundConstructor
+    public TeamBasedAuthorizationStrategy() {
+        teamManager = new TeamManager();
+    }
+
+    public TeamManager getTeamManager() {
+        return teamManager;
+    }
+
+    /**
+     * Get the root ACL which has grand authority over all model level ACLs
+     *
+     * @return root ACL, obtained from Team nager
+     */
+    @Override
+    public ACL getRootACL() {
+        return teamManager.getRoolACL();
+    }
+
+    /**
+     * Get the specific ACL for jobs.
+     *
+     * @param job The access-controlled job
+     * @return The job specific ACL
+     */
+    @Override
+    public ACL getACL(Job<?, ?> job) {
+        return teamManager.getACL(job);
+    }
+
+    public ACL getACL(Team team) {
+        return teamManager.getACL(team);
+    }
+
+    /**
+     * Used by the container realm.
+     *
+     * @return empty List
+     */
+    @Override
+    public Collection<String> getGroups() {
+        return Collections.EMPTY_LIST;
+    }
+
+    @Extension
+    public static final class TeamBasedAuthorizationStrategyDescriptor extends Descriptor<AuthorizationStrategy> {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.TeamBasedAuthorizationStrategy_DisplayName();
+        }
+    }
+
+    public static class ConverterImpl implements Converter {
+
+        @Override
+        public boolean canConvert(Class type) {
+            return type == TeamBasedAuthorizationStrategy.class;
+        }
+
+        @Override
+        public void marshal(Object o, HierarchicalStreamWriter writer, MarshallingContext mc) {
+        }
+
+        @Override
+        public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext uc) {
+            return new TeamBasedAuthorizationStrategy();
+        }
+        
+    }
+}
