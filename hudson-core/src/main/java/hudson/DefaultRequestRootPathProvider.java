3bb4100d6c094f58e0a67709f0989baaf908d8bd
Winston Prakash <winston.prakash@gmail.com>
2013-07-02 20:21:05 -0700

    Fix: 410845 - Migrating server from 3.0.1 to 3.1.0 fails with NPE

--- hudson-core/src/main/java/hudson/DefaultRequestRootPathProvider.java
+++ hudson-core/src/main/java/hudson/DefaultRequestRootPathProvider.java
@@ -4,17 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 
 public class DefaultRequestRootPathProvider implements RequestRootPathProvider {
 
-    
     private static final String REQUEST_ROOT_PATH = "X-Request-Root-Path";
 
-	
-	@Override
-	public String getRootPath(HttpServletRequest req) {
-		  String rootPath = req.getHeader(REQUEST_ROOT_PATH);
-	        if (rootPath == null) {
-	            rootPath = req.getContextPath();
-	        }
-	        return rootPath; 
-	}
-
+    @Override
+    public String getRootPath(HttpServletRequest req) {
+        if (req != null) {
+            String rootPath = req.getHeader(REQUEST_ROOT_PATH);
+            if (rootPath == null) {
+                rootPath = req.getContextPath();
+            }
+            return rootPath;
+        } else {
+            return "/";
+        }
+    }
 }
