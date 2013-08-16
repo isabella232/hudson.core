1058cd615f774a933dfd5fbc981dcb97ab64cb6f
Bob Foster <bobfoster@gmail.com>
2013-08-16 11:47:46 -0700

    Fix ER implied by bug 415242; add -file option to create-job

--- hudson-core/src/main/java/hudson/cli/CreateJobCommand.java
+++ hudson-core/src/main/java/hudson/cli/CreateJobCommand.java
@@ -22,8 +22,13 @@ import static hudson.cli.UpdateJobCommand.ensureJobInTeam;
 import static hudson.cli.UpdateJobCommand.validateTeam;
 import hudson.model.Item;
 import hudson.model.TopLevelItem;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import org.eclipse.hudson.security.team.Team;
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 
 /**
  * Creates a new job by reading stdin as a configuration XML file.
@@ -35,12 +40,14 @@ public class CreateJobCommand extends CLICommand {
 
     @Override
     public String getShortDescription() {
-        return "Creates a new job by reading stdin as a configuration XML file";
+        return "Creates a new job by reading stdin or file as a configuration XML file";
     }
     @Argument(metaVar = "NAME", usage = "Name of the job to create. The job name should not be team  qualified. Ex: job1.", required = true)
     public String name;
     @Argument(metaVar = "TEAM", usage = "Team to create the job in. Optional.", index = 1, required = false)
     public String team;
+    @Option(name = "-file", usage = "Read config.xml from file rather than standard input.")
+    public String file;
 
     protected int run() throws Exception {
         Hudson h = Hudson.getInstance();
@@ -55,8 +62,27 @@ public class CreateJobCommand extends CLICommand {
             stderr.println("Job '" + name + "' already exists");
             return -1;
         }
-
-        TopLevelItem newItem = h.createProjectFromXML(name, stdin);
+        
+        InputStream xml = stdin;
+        if (file != null) {
+            File inputFile = new File(file);
+            if (!inputFile.exists()) {
+                stderr.println("File '" + file + "' does not exist");
+                return -1;
+            }
+            if (!inputFile.isFile()) {
+                stderr.println("File '" + file + "' is not a file");
+                return -1;
+            }
+            try {
+                xml = new FileInputStream(inputFile);
+            } catch (FileNotFoundException e) {
+                stderr.println("File '" + file + "' not found");
+                return -1;
+            }
+        }
+        
+        TopLevelItem newItem = h.createProjectFromXML(name, xml);
         newItem = h.getItem(newItem.getName());
         ensureJobInTeam(newItem, targetTeam, name, stderr);
         return 0;
