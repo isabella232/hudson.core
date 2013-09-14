c78fb57e0ab6c3d6986139a6cdac7ad337b08615
Bob Foster <bobfoster@gmail.com>
2013-09-13 17:04:42 -0700

    Fix Bug 417236 - CLI: create-team: Valid team names are not accepted

--- hudson-core/src/main/java/hudson/cli/CreateTeamCommand.java
+++ hudson-core/src/main/java/hudson/cli/CreateTeamCommand.java
@@ -85,6 +85,6 @@ public class CreateTeamCommand extends CLICommand {
     }
     
     private boolean isAlphaNumeric(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c >= '9');
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
     }
 }
